99217b23ba0969c20231ff12aedab73e5f6e237e
Edward <phatht2911@gmail.com>
2026-05-26 19:15:37 +0700

    chore: add dependencies, update API URL, configure EAS build

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:3000';
+export const API_BASE_URL = 'https://digital-signage-taupe-xi.vercel.app';
 export const API_VERSION = '/api/v1';
 
 export const HEARTBEAT_INTERVAL = 30000;
